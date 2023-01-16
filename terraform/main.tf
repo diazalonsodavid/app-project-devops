64dc63fcecbd443bb47cd48f30b8b76147f6e8bd
David Diaz Alonso <david@sercasystems.com>
2023-01-16 20:35:11 +0100

    push

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+resource "digitalocean_kubernetes_cluster" "bootcamp-project" {
+  name    = "bootcamp-project"
+  region  = "fra1"
+  version = "1.25.4-do.0"
+  ha = true # Provides a high availability (HA) option that provides 99.5% SLA uptime creating multiple backup replicas of each control plane component.
+
+  node_pool {
+    name       = "autoscale-worker-pool"
+    size       = "s-2vcpu-2gb"
+    auto_scale = true
+    min_nodes  = 1
+    max_nodes  = 2
+  }
+}
